e50f173f1da1530eacff56550c2ead647b46841d
Mohammad Ali <mdali2016.227@gmail.com>
2022-02-02 17:57:53 +0600

    [Change] style

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='login h-screen flex justify-center items-center'>
     <div class='shadow-md border-2 rounded-md p-3'>
-      <div class='font-black text-3xl flex justify-center gap-1.5 p-3'>
+      <div class='font-bold text-3xl flex justify-center gap-1.5 p-3'>
         <img src='/favicon.ico' alt='' width='30' height='20'> Invatator
       </div>
       <div class='text-center text-gray-400 p-3'><code> < Let's start software engineering. /> </code></div>
