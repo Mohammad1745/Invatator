abebdbec79a4f1ad8e467ceff85575f00df250a4
Mohammad Ali <mdali2016.227@gmail.com>
2022-02-02 16:06:03 +0600

    [Add] SocialLoginLinks component for login page

---
+++ components/pages/login/SocialLoginLinks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='flex justify-center gap-5'>
+    <a href='http://192.168.0.103:8000/' :class='googleLinkClass'><i class="fab fa-google"></i></a>
+    <a href='http://192.168.0.103:8000/' :class='facebookLinkClass'><i class="fab fa-facebook-f"></i></a>
+    <a href='http://192.168.0.103:8000/' :class='githubLinkClass'><i class="fab fa-github-square"></i></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialLoginLinks',
+  data() {
+    return {
+      googleLinkClass: 'border-b-4 hover:border-red-400 text-3xl text-red-400 pt-0.5',
+      facebookLinkClass: 'border-b-4 hover:border-blue-500 text-3xl text-blue-500 pt-0.5',
+      githubLinkClass: 'border-b-4 hover:border-black text-4xl',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,27 +1,25 @@
 <template>
   <div class='h-screen flex justify-center items-center'>
     <div class='shadow-md border-2 rounded-md p-3'>
-      <div class='font-black text-3xl text-center p-3'>
-        Invatator
+      <div class='font-black text-3xl flex justify-center gap-1.5 p-3'>
+        <img src='/favicon.ico' alt='' width='30' height='20'> Invatator
       </div>
-      <div class='text-center p-3'>
-        Let's start software engineering.
-      </div>
-      <div class='text-center mt-3 p-3'>
-        Login With
-      </div>
-      <div class='flex justify-center gap-3'>
-        <a href='http://127.0.0.1:8000/' class='text-2xl text-red-400 pt-0.5'><i class="fab fa-google"></i></a>
-        <a href='http://127.0.0.1:8000/' class='text-2xl text-blue-500 pt-0.5'><i class="fab fa-facebook-f"></i></a>
-        <a href='http://127.0.0.1:8000/' class='text-3xl'><i class="fab fa-github-square"></i></a>
-      </div>
+      <div class='text-center text-gray-400 p-3'>Let's start software engineering.</div>
+      <div class='text-center text-lg  mt-3 p-3'>Login With</div>
+
+      <social-login-links />
     </div>
   </div>
 </template>
 
 <script>
+import SocialLoginLinks from '../components/pages/login/SocialLoginLinks'
 export default {
-  name: 'login'
+  name: 'login',
+  components: { SocialLoginLinks },
+  head: {
+    title: 'Login'
+  }
 }
 </script>
 
