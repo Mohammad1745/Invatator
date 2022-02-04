d051703bfa8c2dbb86f6419d7fa6f98a2b3bc12b
Mohammad Ali <mdali2016.227@gmail.com>
2022-02-05 00:37:20 +0600

    [Add] linkedin & github authentication

--- components/pages/login/SocialLoginLinks.vue
+++ components/pages/login/SocialLoginLinks.vue
@@ -1,8 +1,8 @@
 <template>
   <div class='flex justify-center gap-5'>
-    <a href='http://192.168.0.103:8000/auth/redirect' :class='googleLinkClass'><i class="fab fa-google"></i></a>
-    <a href='http://192.168.0.103:8000/' :class='facebookLinkClass'><i class="fab fa-facebook-f"></i></a>
-    <a href='http://192.168.0.103:8000/' :class='githubLinkClass'><i class="fab fa-github-square"></i></a>
+    <a href='http://192.168.0.103:8000/auth/redirect?provider=google' :class='googleLinkClass'><i class="fab fa-google"></i></a>
+    <a href='http://192.168.0.103:8000/auth/redirect?provider=linkedin' :class='linkedinLinkClass'><i class="fab fa-linkedin"></i></a>
+    <a href='http://192.168.0.103:8000/auth/redirect?provider=github' :class='githubLinkClass'><i class="fab fa-github-square"></i></a>
   </div>
 </template>
 
@@ -12,7 +12,7 @@ export default {
   data() {
     return {
       googleLinkClass: 'border-b-4 hover:border-red-400 text-3xl text-red-400 pt-0.5',
-      facebookLinkClass: 'border-b-4 hover:border-blue-500 text-3xl text-blue-500 pt-0.5',
+      linkedinLinkClass: 'border-b-4 hover:border-blue-500 text-4xl text-blue-500',
       githubLinkClass: 'border-b-4 hover:border-black text-4xl',
     }
   }
